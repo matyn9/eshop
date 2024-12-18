4f39309eb0dfb4a0d2c32c11ed24cddf3e5e7f94
matyn9 <heller.maty@gmail.com>
2024-12-18 21:27:31 +0100

    finalni commit

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,14 @@ import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import {Dropdown} from 'bootstrap';
 
+// Select the video element
+const video = document.getElementById('myVideo');
+
+// Set the video to autoplay and loop
+video.autoplay = true;
+video.loop = true;
+
+// If autoplay doesn't work, you can also trigger it using JavaScript
+video.play().catch(function(error) {
+    console.log('Autoplay failed: ', error);
+});
